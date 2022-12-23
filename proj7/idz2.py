4112f5547c688112e338d87511954832f1050ddb
MaxDrill <konyjeff@yandex.ru>
2022-12-23 13:00:12 +0300

    fix idz2

--- proj7/idz2.py
+++ proj7/idz2.py
@@ -5,17 +5,25 @@ import sys
 if __name__ == '__main__':
     a = list(map(float, input("Input list: ").split()))
     a1 = []
-    min_a = a[0]
     min_i = 0
     s = 0
 
-    for i in range(0, len(a)):
-        if a[i] < min_a:
-            min_a = a[i]
+    if not a:
+        print(
+            "заданный список пуст",
+            file=sys.stderr
+        )
+        exit(1)
+
+    min_a = a[0]
+
+    for i, item in enumerate(a, 1):
+        if item < min_a:
+            min_a = item
             min_i = i
 
-    for i in range(0, len(a)):
-        if a[i] < 0:
+    for i, item in enumerate(a):
+        if item < 0:
             a1.append(i)
 
     w1 = a1[0]
@@ -27,4 +35,4 @@ if __name__ == '__main__':
     a.sort(key=abs)
 
     print(' '.join(map(str, a)))
-    print("Index of min elem:", min_i, "\n", "Sum elem:", s)
+    print("Num of min elem:", min_i, "\n", "Sum elem:", s)
